4346b84c50353baa3104ce752a881d89283a4241
leneon <kamalnadoh@gmail.com>
2024-11-11 22:42:39 +0100

    first commit

---
+++ src/main/java/com/example/Atiko/resources/UnAuthResource.java
@@ -0,0 +1,99 @@
+package com.example.Atiko.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Atiko.dtos.ArticleDto;
+import com.example.Atiko.dtos.CategorieDto;
+import com.example.Atiko.dtos.CommentaireDto;
+import com.example.Atiko.dtos.NewsletterDto;
+import com.example.Atiko.dtos.StructureDto;
+import com.example.Atiko.services.ArticleService;
+import com.example.Atiko.services.CategorieService;
+import com.example.Atiko.services.CommentaireService;
+import com.example.Atiko.services.NewsletterService;
+import com.example.Atiko.services.StructureService;
+
+@RequestMapping("/unauth")
+@RestController
+public class UnAuthResource {
+    
+    @Autowired
+    private StructureService structureService;
+    @Autowired
+    private ArticleService articleService;
+    @Autowired
+    private CategorieService categorieService;
+    @Autowired
+    private NewsletterService newsletterService;
+    @Autowired
+    private CommentaireService commentaireService;
+
+    @GetMapping("/structure")
+    public ResponseEntity<StructureDto> getFirstStructure() {
+        List<StructureDto> structures = structureService.getAllStructures();
+        if (structures.isEmpty()) {
+            return ResponseEntity.noContent().build();  // 204 No Content si aucune structure n'existe
+        }
+        return ResponseEntity.ok(structures.get(0));  // 200 OK avec la première structure
+    } 
+
+    @GetMapping("/articles")
+    public ResponseEntity<List<ArticleDto>> getAllArticlesWithComments() {
+        List<ArticleDto> aricles = articleService.getAllArticlesWithCommentCount();
+        if (aricles.isEmpty()) {
+            return ResponseEntity.noContent().build();  // 204 No Content si aucune structure n'existe
+        }
+        return ResponseEntity.ok(aricles);  // 200 OK avec la première structure
+    }
+    @GetMapping("/articles/{id}")
+    public ResponseEntity<ArticleDto> getAarticle(@PathVariable Long id) {
+        ArticleDto aricle = articleService.getArticleById(id);
+        if (aricle==null) {
+            return ResponseEntity.noContent().build();  // 204 No Content si aucune structure n'existe
+        }
+        return ResponseEntity.ok(aricle);  // 200 OK avec la première structure
+    }
+    @PutMapping("/like/{id}")
+    public ResponseEntity<ArticleDto> like(@PathVariable Long id) {
+       return ResponseEntity.ok(articleService.like(id));
+    }
+    @PutMapping("/dislike/{id}")
+    public ResponseEntity<ArticleDto> dislike(@PathVariable Long id) {
+       return ResponseEntity.ok(articleService.like(id));
+    }
+    @GetMapping("/categories")
+    public ResponseEntity<List<CategorieDto>> getAllCategories() {
+        List<CategorieDto> cats = categorieService.getAllCategories();
+        if (cats.isEmpty()) {
+            return ResponseEntity.noContent().build();  // 204 No Content si aucune structure n'existe
+        }
+        return ResponseEntity.ok(cats);  // 200 OK avec la première structure
+    }
+
+    @PostMapping("/newsletter")
+    public ResponseEntity<NewsletterDto> newsletter(@RequestBody NewsletterDto dto) {
+        return ResponseEntity.ok(newsletterService.createNewsletter(dto));  // 200 OK avec la première structure
+    }
+
+    @PostMapping("/commentaire")
+    public ResponseEntity<CommentaireDto> postComment(@RequestBody CommentaireDto dto) {
+        CommentaireDto comment = commentaireService.saveCommentaire(dto);
+        if (comment==null) {
+            return ResponseEntity.noContent().build();  // 204 No Content si aucune structure n'existe
+        }
+        return ResponseEntity.ok(comment);  // 200 OK avec la première structure
+    }
+
+  
+}
---
+++ src/main/resources/static/assets/js/controllers/unAuthController.js
@@ -0,0 +1,328 @@
+var App = angular.module('myApp', []);
+
+App.controller('unAuthController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+    // URLs pour les opérations CRUD sur les structures
+    const url = '/unauth';
+    const structuresUrl = url + '/structure';
+    const articlesUrl = url + '/articles';
+    const categoriesUrl = url + '/categories';
+    const newslettersUrl = url + '/newsletter';
+    const commentaireUrl = url + '/commentaire';
+    const contactUrl = url + '/contact';
+
+    // Initialisation des variables
+    $scope.newsletter = null;
+    $scope.firstArtcile = null;
+    $scope.firstArtciles = null;
+    $scope.lastArtciles = [];
+    $scope.singleArticle = {id:null,description:null, couverture:null,categorie:{}};
+    $scope.commentaireDto = {id:null,contenue:null, useremail:null,username:null, articleId:null, createdAt:null};
+    $scope.commentaireMasterDto = {id:null,contenue:null, useremail:null,username:null, articleId:null, createdAt:null};
+    $scope.contactMasterDto = {message:null, useremail:null,username:null,subject:null};
+    $scope.contactDto = {message:null, useremail:null,username:null,subject:null};
+    $scope.newsletterDto = {useremail:null};
+    $scope.listeCategories = [];
+    $scope.listeArticlesFilter = [];
+    $scope.listeArticles = [];
+    $scope.structureDto = {
+        id: null,
+        nom: null,
+        telephone: null,
+        email: null,
+        dateCreation: null,
+        localisation: null,
+        gps: null,
+        logo: null,
+        bio: null,
+        description: null,
+        img: null,
+        img1: null,
+        img2: null,
+        facbook: null,
+        twitter: null,
+        youtube: null,
+        instagram: null
+
+    };     
+
+    // Fonction pour charger la liste des structures
+    $scope.loadStructure = function () {
+        $http.get(structuresUrl)
+            .then(function (res) {
+                $scope.structureDto = res.data;
+                $scope.structureDto.dateCreation = new Date($scope.structureDto.dateCreation);
+                $scope.structureDto.description = $sce.trustAsHtml($scope.structureDto.description);
+                $scope.structureDto.bio = $sce.trustAsHtml($scope.structureDto.bio);
+            })
+            .catch(function (error) {
+                console.error("ERREUR DE RECUPERATION DES DONNEES : ", error);
+            });
+    };
+    // Chargement des structures au chargement de la page
+    $scope.loadStructure();
+
+    $scope.loadarticles = function () {
+        $http.get(articlesUrl)
+            .then(function (res) {
+                // Traitement de la liste des articles
+                $scope.listeArticles = res.data.map(article => ({
+                    ...article,
+                    contenue: $sce.trustAsHtml(article.contenue),
+                    createdAt: new Date(article.createdAt)
+                }));
+    
+                // Sélection du premier article
+                $scope.firstArticle = $scope.listeArticles[0] ;
+                $scope.listeArticlesFilter = $scope.listeArticles.slice(0, 4);
+                // Sélection des trois premiers articles
+                $scope.firstArticles = $scope.listeArticles.slice(0, 3);
+    
+                // Sélection des quatre dernier s articles
+                $scope.lastArticles = $scope.listeArticles.slice(-5);
+                console.log($scope.firstArticle);
+            })
+            .catch(function (error) {
+                console.error("ERREUR DE RECUPERATION DES DONNEES : ", error);
+            });
+    };
+    // Chargement des structures au chargement de la page
+    $scope.loadarticles();
+
+    // Fonction pour charger la liste des structures
+    $scope.loadCateories = function () {
+        $http.get(categoriesUrl)
+            .then(function (res) {
+                $scope.listeCategories = res.data;
+                console.log("LISTE DE CATEGORIES    :",$scope.listeCategories );
+            })
+            .catch(function (error) {
+                console.error("ERREUR DE RECUPERATION DES CATEGORIES : ", error);
+            });
+    };
+    // Chargement des structures au chargement de la page
+    $scope.loadCateories();
+
+    $scope.filterByCategory = function(categoryName) {
+        if (categoryName) {
+            // Filtrer et limiter à 4 articles
+            $scope.listeArticlesFilter = $scope.listeArticles
+                .filter(function(article) {
+                    return article.categorie.nom === categoryName;
+                })
+                .slice(0, 4); // Limiter aux 4 premiers
+        } else {
+            // Si aucune catégorie n'est sélectionnée, afficher les 4 premiers articles
+            $scope.listeArticlesFilter = $scope.listeArticles.slice(0, 4);
+        }
+    };
+    
+    // Fonction pour charger la liste des structures
+    const articleIdElement = document.getElementById("articleId");
+
+    $scope.loadArticle = function (articleId) {
+        $http.get(articlesUrl+"/"+articleId)
+            .then(function (res) {
+                $scope.singleArticle = res.data;
+                $scope.singleArticle.contenue = $sce.trustAsHtml($scope.singleArticle.contenue);
+                $scope.singleArticle.dateCreation = new Date($scope.singleArticle.dateCreation);
+                console.log($scope.singleArticle);
+            })
+            .catch(function (error) {
+                console.error("ERREUR DE RECUPERATION DES CATEGORIES : ", error);
+            });
+    };
+    // Chargement des structures au chargement de la page
+    if (articleIdElement && articleIdElement.value) {
+        const articleId = articleIdElement.value;
+        console.log("Article ID:", articleId);
+        $scope.loadArticle (articleId);
+    }
+
+    $scope.submitFeedback = function(liked) {
+        const url = "/unauth/" + (liked? "like/" : "dislike/") + $scope.singleArticle.id;
+        $http.put(url)
+        .then(function (res) {
+            $scope.feedbackMessage = liked 
+            ? "Merci ! Heureux de savoir que vous avez aimé cet article."
+            : "Merci pour votre retour ! Nous ferons de notre mieux pour nous améliorer.";
+        })
+        .catch(function (error) {
+            console.error("ERREUR DE RECUPERATION DES CATEGORIES : ", error);
+        });
+        
+    };
+
+    $scope.newsletter = function() {
+        if(!$scope.newsletterDto.useremail)
+            return null;
+        fetch(newslettersUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify($scope.newsletterDto)
+        })
+        .then(response => {
+            if (response.status === 409) {
+                throw new Error("Une erreur est survenue. Veuillez réessayer.");
+            } else if (!response.ok) {
+                throw new Error("Cet email est déjà inscrit à la newsletter.");
+            }
+            return response.json();
+        })
+        .then(data => {
+            
+            Swal.fire({
+                title: 'Félicitations !',
+                text: "Vous êtes maintenant abonné et au bon endroit pour rester informé des dernières nouveautés !",
+                icon: 'success',
+                confirmButtonText: 'Merci !'
+            });
+        })
+        .catch(error => {
+            Swal.fire({
+                title: 'Erreur',
+                text: error.message,
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        });
+        $scope.newsletterDto.useremail = null;   
+        
+    }; 
+    $scope.comment = function() {
+        $scope.commentaireDto.articleId = articleIdElement.value;
+        
+        if(!$scope.commentaireDto.username || !$scope.commentaireDto.username){
+        return null;
+        }
+
+        fetch(commentaireUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: angular.toJson($scope.commentaireDto)
+        })
+        .then(response => {
+            if (response.status === 409) {
+                throw new Error("Une erreur est survenue. Veuillez réessayer.");
+            }
+            return response.json();
+        })
+        .then(data => {
+            Swal.fire({
+                title: 'Félicitations !',
+                text: "Merci d'avoir commenté ce post!",
+                icon: 'success',
+                confirmButtonText: 'Merci !'
+            });
+        })
+        .catch(error => {
+            Swal.fire({
+                title: 'Erreur',
+                text: error.message,
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        });
+        
+        $scope.commentaireDto = angular.copy($scope.commentaireMasterDto);
+
+    };
+
+    $scope.contact = function() {
+        
+        if(!$scope.contactDto.username || !$scope.contactDto.useremail || contactDto.message || contactDto.subject){
+            return null;
+        }
+
+        fetch(contactUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: angular.toJson($scope.contactDto)
+        })
+        .then(response => {
+            if (response.status === 409) {
+                throw new Error("Une erreur est survenue. Veuillez réessayer.");
+            }
+            return response.json();
+        })
+        .then(data => {
+            Swal.fire({
+                title: 'Félicitations !',
+                text: "Merci de nous avoir contacté, votre requette serais traité dans les bref delais!",
+                icon: 'success',
+                confirmButtonText: 'Merci !'
+            });
+        })
+        .catch(error => {
+            Swal.fire({
+                title: 'Erreur',
+                text: error.message,
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        });
+        
+        $scope.commentaireDto = angular.copy($scope.commentaireMasterDto);
+
+    };
+
+    $scope.listeArticles = function(limit) {
+        // Supposons que `articles` soit une liste d'articles déjà récupérée
+        // Trier les articles par createdAt dans l'ordre décroissant
+        const sortedArticles = $scope.listeArticles.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+        
+        // Retourner les 5 premiers articles
+        return sortedArticles.slice(0, limit);
+    };
+
+    // Méthode pour obtenir l'article précédent
+    $scope.prevPost = function(currentId) {
+        const currentIndex = $scope.listeArticles.findIndex(article => article.id === currentId);  
+        // Si l'article précédent existe, le retourner
+        if (currentIndex > 0) {
+            return $scope.listeArticles[currentIndex - 1];
+        }          // Votre logique continue ici
+        return null; // Retourne null si c'est le premier article
+    };
+
+
+    $scope.nextPost = function(currentId) {
+            const currentIndex = $scope.listeArticles.findIndex(article => article.id === currentId);
+            if (currentIndex < $scope.listeArticles.length - 1) {
+                return $scope.listeArticles[currentIndex + 1];
+            }                // Votre logique continue ici
+        return null; // Retourne null si c'est le dernier article
+    };
+
+
+$scope.currentPage = 1;
+$scope.pageSize = 5; // Nombre d'articles par page
+$scope.totalPages = Math.ceil($scope.totalArticles / $scope.pageSize); // Total de pages
+
+$scope.setPage = function (page) {
+    if (page >= 1 && page <= $scope.totalPages) {
+        $scope.currentPage = page;
+        $scope.loadArticles(); // Appel de la fonction pour charger les articles de la page sélectionnée
+    }
+};
+
+// Fonction de chargement des articles pour la page active
+$scope.loadArticles = function() {
+    // Requête pour récupérer les articles de la page `$scope.currentPage`
+    // Exemple :
+    // fetch(`api/articles?page=${$scope.currentPage}&size=${$scope.pageSize}`)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         $scope.articles = data.articles;
+    //         $scope.totalArticles = data.totalCount;
+    //         $scope.totalPages = Math.ceil($scope.totalArticles / $scope.pageSize);
+    //         $scope.$apply();
+    //     });
+};
+
+// Initialisation au chargement
+$scope.loadArticles();
+
+    
+}]);
+
+
